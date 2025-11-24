76796f2578b66c58e7ec4e2a26936b2c78d2b6ee
21223232326 <Olvia.Letyagina@gmail.com>
2025-11-24 20:19:09 +0200

    ttt

---
+++ js/guess-the-number.js
@@ -0,0 +1,45 @@
+const secretNumber = Math.floor(Math.random() * 10) + 1;
+let guesses = 0;
+
+function checkGuess()
+{
+  const guessInput = document.getElementById("guessInput");
+  const guess = parseInt(guessInput.value);
+  
+  if (isNaN(guess) || guess < 1 || guess > 10)
+  {
+    displayOutput("Помилка введення числа! Введіть число від 1 до 10!");
+    return;
+  }
+  
+  guesses++;
+  
+  if (guess == secretNumber)
+  {
+    displayOutput("Поздоровляємо! Ти вгадав число " + secretNumber + " за " + guesses + " спроб/и!");
+    guessInput.disabled = true;
+    guessInput.placeholder = "Гра завершена";
+    guessInput.classList.add("disabled");
+  }
+  else if (guess < secretNumber)
+  {
+    displayOutput("Загадане число більше!");
+  }
+  else
+  {
+    displayOutput("Загадане число менше!");
+  }
+  guessInput.value = "";
+  guessInput.focus();
+}
+
+function displayOutput(message)
+{
+  const output = document.getElementById("output");
+  output.textContent = message;
+  output.classList.add("animated", "fadeIn");
+  setTimeout(() =>
+  {
+    output.classList.remove("animated", "fadeIn");
+  }, 500);
+  }
